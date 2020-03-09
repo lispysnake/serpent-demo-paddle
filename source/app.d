094a4475746812dccebaf4da6b2cff94f288fc48
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-03-09 00:51:09 +0000

    Initial code structure
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ source/app.d
@@ -0,0 +1,56 @@
+/*
+ * This file is part of serpent.
+ *
+ * Copyright © 2019-2020 Lispy Snake, Ltd.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+import serpent;
+import serpent.graphics.sprite;
+
+/* Simple no-op app */
+class MyApp : serpent.App
+{
+
+    Scene scene;
+
+    final override bool bootstrap(View!ReadWrite view)
+    {
+        scene = new Scene("default");
+        context.display.addScene(scene);
+        scene.addCamera(new OrthographicCamera());
+        return true;
+    }
+}
+
+/* Main entry */
+void main()
+{
+    auto context = new serpent.Context();
+    context.display.pipeline.driverType = DriverType.Vulkan;
+    context.display.size(1366, 768);
+    context.display.title = "#serpent Paddle Demo";
+
+    /* TODO: Remove need for casts! */
+    import serpent.graphics.pipeline.bgfx;
+
+    auto pipe = cast(BgfxPipeline) context.display.pipeline;
+    pipe.addRenderer(new SpriteRenderer());
+
+    context.run(new MyApp);
+}
